05d9b7901b5a21c1c7577038bb68a8a1e2b28058
Sudhriti <sinha733215@gmail.com>
2024-07-14 12:29:23 +0530

    Deployment ready

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -12,20 +12,23 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)o-@ov7fu9z5zw+_wg5t)*4-kw#woqe4l#l@1r3^9=jt&qbl(-'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['bookstore.onrender.com'])
 
 
 # Application definition
